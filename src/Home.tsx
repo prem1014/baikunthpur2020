f19164055117f86d29e7dd368eb9eb1360eac5df
Prem Prakash <prem.prakash@capita.co.uk>
2020-09-12 00:07:09 +0530

    latest changes

--- src/Home.tsx
+++ src/Home.tsx
@@ -22,6 +22,10 @@ const Home = () => {
     const title = '99 बैकुंठपुर विधानसभा';
     const [isPanchayat, setIsPanchayat] = useState(false);
     const [village, setVillage] = useState('');
+    const [userName, setUserName] = useState('');
+    const [mobileNo, setMobileNo] = useState('');
+    const [party, setParty] = useState('');
+    const [isOpen, setIsOpen] = useState(false)
     let vote: any = {
         rjd: [],
         jdu: [],
@@ -34,7 +38,7 @@ const Home = () => {
     const [votes, setVotes] = useState(vote);
 
     const inputChange = (e: any) => {
-        setIsPanchayat(e.target.value !== defaultValue ? true: false);
+        setIsPanchayat(e.target.value !== defaultValue ? true : false);
         setVillage(e.target.value);
     }
 
@@ -50,73 +54,103 @@ const Home = () => {
     }
 
     const submit = (party: string) => {
+        if (!mobileNo) {
+            alert('मोबाइल नंबर दीजिये');
+            return;
+        }
+        if (mobileNo.length !== 10) {
+            alert('मोबाइल नंबर वैध नहीं है। ');
+            return;
+        }
         if(document.cookie || localStorage.getItem('id')) {
             alert('आपके मोबाइल या कंप्यूटर से एक बार वोट हो चूका है। कृपया दूसरे मोबाइल या कंप्यूटर से कोसिस करें। ')
             return;
         }
         const obj = {
-            name: 'unknown',
             status: true,
-            village: village
+            village: village,
+            name: userName,
+            mobile: mobileNo
         }
 
         let voteShareCopy = { ...votes };
         voteShareCopy[party].push(obj);
         voteShareCopy.total = voteShareCopy.rjd.length + voteShareCopy.bjp.length + voteShareCopy.jdu.length + voteShareCopy.oth.length
         setVotes(voteShareCopy);
-        axios.post('https://nrf-api.herokuapp.com/api/mla', {feedback: {...voteShareCopy, _id: party}})
-        .then( res => {
-            alert(res.data.message);
-            getVoteFromDB();
-            localStorage.setItem('id',  '' + new Date().getTime());
-            document.cookie = '' + new Date().getTime();
-        })
-        .catch( err => {
+        axios.post('https://nrf-api.herokuapp.com/api/mla', { feedback: { ...voteShareCopy, _id: party } })
+            .then(res => {
+                alert(res.data.message);
+                getVoteFromDB();
+                localStorage.setItem('id', '' + new Date().getTime());
+                document.cookie = '' + new Date().getTime();
+                onclose();
+            })
+            .catch(err => {
 
-        })
+            })
     }
 
     const getVotePer = (vote: number, total: number) => {
-        if(total === 0) return total;
+        if (total === 0) return total;
 
 
-        return (((vote/total) * 100)).toFixed(1) + '%'
+        return (((vote / total) * 100)).toFixed(1) + '%'
     }
 
     const getVoteFromDB = () => {
         axios.get('https://nrf-api.herokuapp.com/api/mla')
-        .then( res => {
-            console.log(res.data);
-            const rjd = res.data.result.filter( (it: any) => it._id === 'rjd')
-            const jdu = res.data.result.filter( (it: any) => it._id === 'jdu')
-            const bjp = res.data.result.filter( (it: any) => it._id === 'bjp')
-            const oth = res.data.result.filter( (it: any) => it._id === 'oth')
-            const voteData = {
-                rjd: rjd[0].rjd,
-                jdu: jdu[0].jdu,
-                bjp: bjp[0].bjp,
-                oth: oth[0].oth,
-                total: 0
-            }
-            voteData.total = voteData.rjd.length + voteData.jdu.length + voteData.bjp.length + voteData.oth.length;
-            setVoteShare(voteData);
-        })
-        .catch( err => {
+            .then(res => {
+                console.log(res.data);
+                // const rjd = res.data.result.filter((it: any) => it._id === 'rjd')
+                // const jdu = res.data.result.filter((it: any) => it._id === 'jdu')
+                // const bjp = res.data.result.filter((it: any) => it._id === 'bjp')
+                // const oth = res.data.result.filter((it: any) => it._id === 'oth')
+                const voteData = {
+                    rjd: res.data.result.rjd,
+                    jdu: res.data.result.jdu,
+                    bjp: res.data.result.bjp,
+                    oth: res.data.result.oth,
+                    total: 0
+                }
+                voteData.total = voteData.rjd + voteData.jdu + voteData.bjp + voteData.oth;
+                debugger
+                setVoteShare(voteData);
+            })
+            .catch(err => {
 
-        })
+            })
     }
 
     const getVoteByVillage = (id: any, party: any) => {
-        const total = voteShare[party].filter( (itm: any) => itm.village === id);
+        const total = voteShare[party].filter((itm: any) => itm.village === id);
 
         return total.length;
     }
 
     const getVillageById = (id: string) => {
-        const villageDetails: any = listOfPanchayat.find( (vill: any) => vill.id === id);
+        const villageDetails: any = listOfPanchayat.find((vill: any) => vill.id === id);
         return villageDetails.hn
     }
-    useEffect( () => {
+
+    const inputHndl = (e: any, type: string) => {
+        if (type === 'name') {
+            setUserName(e.target.value);
+        } else if (type === 'mobile') {
+            setMobileNo(e.target.value);
+        }
+    }
+
+    const openModal = (party: string) => {
+        setParty(party);
+        setIsOpen(true)
+    }
+
+    const onclose = () => {
+        setIsOpen(false);
+        setParty('');
+    }
+
+    useEffect(() => {
         getVoteFromDB();
     }, [])
 
@@ -146,50 +180,83 @@ const Home = () => {
             {
                 isPanchayat &&
                 <>
-                     <hr />
-                     <h3 className="text-center">Total Vote {voteShare.total} (सर्वे 18-Sept-2020  को  समाप्त हो जायेगा। ) </h3>
+                    <hr />
+                    <h3 className="text-center">Total Vote {voteShare.total} (सर्वे 18-Sept-2020  को  समाप्त हो जायेगा। ) </h3>
                     <div className="row">
-                        <div className="col-lg-3 col-md-3 col-6" style={{marginBottom: '10px'}}>
-                            <div style={container} onClick={(event) => submit('rjd')}>
+                        <div className="col-lg-3 col-md-3 col-6" style={{ marginBottom: '10px' }}>
+                            <div style={container} onClick={(event) => openModal('rjd')}>
                                 <h5 className="center"><img src={rjd} style={rjdtyle} /></h5>
                                 <h5 className="text-center">प्रेम शंकर यादव</h5>
-                                <h2>{voteShare.rjd.length}</h2>
-                                <h3>{getVotePer(voteShare.rjd.length, voteShare.total)}</h3>
-                                <h6>{getVillageById(village)} {getVoteByVillage(village, 'rjd')}</h6>
+                                <h2>{voteShare.rjd}</h2>
+                                <h3>{getVotePer(voteShare.rjd, voteShare.total)}</h3>
+                                {/* <h6>{getVillageById(village)} {getVoteByVillage(village, 'rjd')}</h6> */}
                             </div>
                         </div>
-                        <div className="col-lg-3 col-md-3 col-6" style={{marginBottom: '10px'}}>
-                        <div style={container} onClick={(event) => submit('jdu')}>
+                        <div className="col-lg-3 col-md-3 col-6" style={{ marginBottom: '10px' }}>
+                            <div style={container} onClick={(event) => openModal('jdu')}>
                                 <h5 className="center"><img src={jdu} style={rjdtyle} /></h5>
                                 <h5 className="text-center">मंजीत सिंह</h5>
-                                <h2>{voteShare.jdu.length}</h2>
-                                <h3>{getVotePer(voteShare.jdu.length, voteShare.total)}</h3>
-                                <h6>{getVillageById(village)} {getVoteByVillage(village, 'jdu')}</h6>
+                                <h2>{voteShare.jdu}</h2>
+                                <h3>{getVotePer(voteShare.jdu, voteShare.total)}</h3>
+                                {/* <h6>{getVillageById(village)} {getVoteByVillage(village, 'jdu')}</h6> */}
                             </div>
-                            </div>
+                        </div>
                         <div className="col-lg-3 col-md-3 col-6">
-                        <div style={container} onClick={(event) => submit('bjp')}>
+                            <div style={container} onClick={(event) => openModal('bjp')}>
                                 <h5 className="center"><img src={bjp} style={rjdtyle} /></h5>
                                 <h5 className="text-center">मिथलेश तिवारी</h5>
-                                <h2>{voteShare.bjp.length}</h2>
-                                <h3>{getVotePer(voteShare.bjp.length, voteShare.total)}</h3>
-                                <h6>{getVillageById(village)} {getVoteByVillage(village, 'bjp')}</h6>
+                                <h2>{voteShare.bjp}</h2>
+                                <h3>{getVotePer(voteShare.bjp, voteShare.total)}</h3>
+                                {/* <h6>{getVillageById(village)} {getVoteByVillage(village, 'bjp')}</h6> */}
+                                
                             </div>
-                            </div>
+                        </div>
                         <div className="col-lg-3 col-md-3 col-6">
-                        <div style={container} onClick={(event) => submit('oth')}>
+                            <div style={container} onClick={(event) => openModal('oth')}>
                                 <h5 className="center"><img src={oth} style={rjdtyle} /></h5>
                                 <h5 className="text-center">कोई और</h5>
-                                <h2>{voteShare.oth.length}</h2>
-                                <h3>{getVotePer(voteShare.oth.length, voteShare.total)}</h3>
-                                <h6>{getVillageById(village)} {getVoteByVillage(village, 'oth')}</h6>
+                                <h2>{voteShare.oth}</h2>
+                                <h3>{getVotePer(voteShare.oth, voteShare.total)}</h3>
+                                {/* <h6>{getVillageById(village)} {getVoteByVillage(village, 'oth')}</h6> */}
                             </div>
-                            </div>
+                        </div>
                     </div>
                 </>
             }
+            {
+                isOpen &&
+                <div id="myModal" className="cus-modal">
+                <div className="cus-modal-content">
+                    <span className="close" onClick={() => onclose()}>&times;</span>
+                    <div className="row" style={{ marginTop: '40px' }}>
+                        <div className="col-12">
+                            <input type="text"
+                                value={userName}
+                                placeholder="अपना नाम डालिये।"
+                                onChange={(e) => inputHndl(e, 'name')}
+                                className="form-control"
+                            />
+                        </div>
+                        <div className="col-12" style={{ marginTop: '10px' }}>
+                            <input type="text"
+                                value={mobileNo}
+                                placeholder="मोबाइल नंबर दीजिये।"
+                                onChange={(e) => inputHndl(e, 'mobile')}
+                                className="form-control"
+                            />
+                        </div>
+                        <div className="col-12" style={{ marginTop: '10px' }}>
+                            <button disabled={(!userName)}
+                                onClick={(event) => submit(party)}
+                                className="btn btn-info"
+                            >वोट कीजिये</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            }
             <div className="row">
-                <div className="col-12" style={{...container, marginTop: '20px'}}>
+                <div className="col-12" style={{ ...container, marginTop: '20px' }}>
                     <h5><a href="http://premprakash.co.in/">Designed and developed by Prem Prakash</a></h5>
                 </div>
             </div>
